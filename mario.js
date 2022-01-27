1510a5defb16bccd00d46948dbcef93973a6ac56
trathe <78220454+trathe9977@users.noreply.github.com>
2022-01-27 00:33:12 +0000

    render the pyramid to the DOM instead of the console

--- mario.js
+++ mario.js
@@ -6,8 +6,6 @@ printPyramid(height);
 // TODO #2
 // Take in user input for the height
 
-let space = " ";
-let brick = "#";
 
 let str = '';
 /*
@@ -27,21 +25,23 @@ function printPyramid(height) {
   for(let i=0; i<rows; i++) {  //outer loop
     
     let str = '';
-    for(let k = 1; k <= rows - i; k++){
-       str += ' ';
+    for(let k = 2; k <= rows - i; k++){
+       str += '.';
      }
 
     for(let j=0; j<=i+1; j++) {//inner loop
 
     str += "#"; //this adds a # to the end of the string.
 
-
+    
 
   }
 
-  console.log(str); //once a row is done we print it out
-    
-
+  let rowStr = document.createElement("p");
+  rowStr.innerHTML = str;
+  document.getElementById("pyramid").appendChild(rowStr);
+  
+  
     // TODO #1
     // print that pyramid!
   }
