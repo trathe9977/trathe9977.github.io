ac4934406a356a30ad4d333ba7a5a1f8e48a8421
trathe <78220454+trathe9977@users.noreply.github.com>
2022-02-14 21:25:24 +0000

    move styles to style.css, add dropdown box for symbol change, add slider for height, change header, remove text box and old text

--- mario.js
+++ mario.js
@@ -1,38 +1,40 @@
 
-let clicker = document.getElementById("drawIt");
-clicker.addEventListener("click", determineHeightAndThenDrawPyramid);
+document.getElementById("pHeight").addEventListener("input", determineHeightAndThenDrawPyramid);
+
+
+
 
 function determineHeightAndThenDrawPyramid() {
 
-  rows = document.getElementById("height").value; 
+  rows = document.getElementById("pHeight").value; 
+  brick = document.getElementById('symbols').value;
 
   printPyramid();
 
 }
 
-function printPyramid(height) {
+function printPyramid(pHeight) {
+  
   document.getElementById("pyramid").innerHTML = "";
 
   for(let i=0; i<rows; i++) {  //outer loop
     
     let str = '';
-      for(let k = 2; k <= rows - i; k++){
+    for(let k = 2; k <= rows - i; k++){
        str += '.';
-      }
+     }
 
-      for(let j=0; j<=i+1; j++) {//inner loop
-      str += "#"; //this adds a # to the end of the string.
+    for(let j=0; j<=i+1; j++) {//inner loop
 
-      }
+    str += brick; //this adds a # to the end of the string.
+
+    }
     
     let rowStr = document.createElement("p");
     rowStr.innerHTML = str;
     document.getElementById("pyramid").appendChild(rowStr);
   
-  
 
-    // TODO #1
-    // print that pyramid!
   }
 
 }
